670e4620f5731f13e7a081c9cac2ab1f9386aefc
Jason-Kyle De Lara <jason8oh8@gmail.com>
2021-08-09 15:44:07 -0700

    Changed Title of index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://react2025.com">React2025</a>
         </h1>
 
         <p className={styles.description}>
